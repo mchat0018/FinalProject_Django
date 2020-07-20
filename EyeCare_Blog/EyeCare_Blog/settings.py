946d9fa4e78754d21f6ae71e772235fb4eb5e4fb
Mihir Chaturvedi <mihir19175@iiitd.ac.in>
2020-07-20 10:12:53 +0530

    Home page now displaying properly, forms to be debugged

--- EyeCare_Blog/EyeCare_Blog/settings.py
+++ EyeCare_Blog/EyeCare_Blog/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     
 ]
 
